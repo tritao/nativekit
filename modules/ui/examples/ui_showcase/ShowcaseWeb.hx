e341eaaff6fe362cfad3ee29f1faea01b04b969e
Joao Matos <joao@tritao.eu>
2026-09-13 01:59:52 +0100

    feat(haxe): use typed keys in web showcase

--- modules/ui/examples/ui_showcase/ShowcaseWeb.hx
+++ modules/ui/examples/ui_showcase/ShowcaseWeb.hx
@@ -4,7 +4,6 @@ import NativeKit.NativeKitConstants;
 import NativeKit.EventKind;
 import NativeKit.GraphicsApi;
 import NativeKit.InputAction;
-import NativeKit.Key;
 import NativeKit.Result;
 import NativeKit.InitOptions;
 import NativeKit.SurfaceOptions;
@@ -157,7 +156,7 @@ class ShowcaseWeb {
                     case PointerButton(source, _, action, _, x, y) if (source == window && app != null):
                         app.pointerButton(x, y, action == InputAction.Press);
                     case Key(source, key, _, action, _) if (source == window &&
-                            action == InputAction.Press && key == Key.Escape):
+                            action == InputAction.Press && key == NativeKitConstants.NK_KEY_ESCAPE):
                         running = false;
                     default:
                 }
