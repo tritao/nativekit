26a32149b143f3624f072938083e6051d42af555
Joao Matos <joao@tritao.eu>
2026-09-20 00:50:41 +0100

    gpu: add compute passes and storage bindings

--- modules/gpu/src/sokol/nativekit_sokol_api.h
+++ modules/gpu/src/sokol/nativekit_sokol_api.h
@@ -21,6 +21,7 @@ typedef struct nk_sokol_api {
     sg_limits (*query_limits)(void);
     void (*update_buffer)(sg_buffer buffer, const sg_range *data);
     void (*apply_viewport)(int x, int y, int width, int height, bool origin_top_left);
+    void (*dispatch)(int num_groups_x, int num_groups_y, int num_groups_z);
     int (*runtime_acquire)(const sg_desc *desc, nk_graphics_device device, uint64_t native_device);
     int (*runtime_is_compatible)(const sg_desc *desc, nk_graphics_device device,
                                  uint64_t native_device);
